903a51dd9656018d411ae9bc636ce022e0f5086a
swagatkoirala <swagatkoirala5@gmail.com>
2021-05-30 21:05:02 +0545

    completed #2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -27,9 +27,9 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
             {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || ''}</span>
+            <span className="text-color-main">{name || 'Swagat Koirala'}</span>
             <br />
-            {subtitle || ""}
+            {subtitle || "Software Developer"}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
